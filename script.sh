0a58ff8eedbec9c9fa91d2f0d407360351869a9c
Ziz <>
2025-01-04 18:43:35 +0100

    final for now

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/sh
 # Checking for filename in argument list
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <filename>"
@@ -10,6 +9,10 @@ output_filename="timestamped_$1"
 
 echo "simulation performance tracking enabled, tracking file $1, output saved in file $output_filename"
 
-echo "$1" | entr -s "stat -c "%y" "$1" >> $output_filename && tail -n 12 $1 | grep "Step" -A 1 >> $output_filename"
-
-
+inotifywait -q -m -e modify . |
+	while read -r directory events filename; do
+		if [ "$filename" = "$1" ]; then
+			stat -c "%z" $1 >> $output_filename;
+			tail -n 12 $1 | grep "Step" -A 1 >> $output_filename
+		fi
+	done
