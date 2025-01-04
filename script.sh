0d577fbc316bf667b1ceee16ad1211907cb9ba67
Ziz <>
2025-01-04 15:51:25 +0100

    works \[T]/

--- script.sh
+++ script.sh
@@ -10,6 +10,6 @@ output_filename="timestamped_$1"
 
 echo "simulation performance tracking enabled, tracking file $1, output saved in file $output_filename"
 
-echo "$1" | entr -s "tail -n 1 | timestamped_$1"
+echo "$1" | entr -s "stat -c "%y" "$1" >> $output_filename && tail -n 12 $1 | grep "Step" -A 1 >> $output_filename"
 
 
