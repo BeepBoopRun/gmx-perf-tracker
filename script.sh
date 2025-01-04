8972063a5f1c54dd641086c29efbd152af9f1830
Ziz <>
2025-01-04 15:31:58 +0100

    working on the script

--- script.sh
+++ script.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "simulation performance tracking on, tracking file $1"
+# Checking for filename in argument list
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+output_filename="timestamped_$1"
+
+echo "simulation performance tracking enabled, tracking file $1, output saved in file $output_filename"
+
+echo "$1" | entr -s "tail -n 1 | timestamped_$1"
+
+
